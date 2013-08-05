f536accbd52013604582ec7303bd9599a4446178
Peter <togipete@hotmail.com>
2013-08-05 01:39:56 -0700

    Cleaned up number checking even more on ex48

--- ex48/ex48/lexicon.py
+++ ex48/ex48/lexicon.py
@@ -2,8 +2,6 @@ directions = ['north', 'south', 'east', 'west', 'down', 'up', 'left', 'right', '
 verbs = ['go', 'stop', 'kill', 'eat']
 stops = ['the', 'in', 'of', 'from', 'at', 'it']
 nouns = ['door', 'bear', 'princess', 'cabinet']
-numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
 
 def scan(string):
     result = []
@@ -18,22 +16,12 @@ def scan(string):
             result.append(('stop', word))
         elif word in nouns:
             result.append(('noun', word))
-        elif is_number(word):
+        elif convert_number(word) != None:
             result.append(('number', int(word)))
         else:
             result.append(('error', word))
 
     return result
-
-def is_number(string):
-    for char in string:
-        c = convert_number(char)
-        if c != None and c in numbers:
-            pass
-        else:
-            return False
-
-    return True
 
 def convert_number(c):
     try:
