20db5d2a0c02014a186a61654b4fee91cfc7925f
Peter <togipete@hotmail.com>
2013-08-05 01:34:36 -0700

    Fixed numericals on exercise 48

--- ex48/ex48/lexicon.py
+++ ex48/ex48/lexicon.py
@@ -2,7 +2,7 @@ directions = ['north', 'south', 'east', 'west', 'down', 'up', 'left', 'right', '
 verbs = ['go', 'stop', 'kill', 'eat']
 stops = ['the', 'in', 'of', 'from', 'at', 'it']
 nouns = ['door', 'bear', 'princess', 'cabinet']
-numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
 def scan(string):
