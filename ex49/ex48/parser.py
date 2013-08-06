db511469d976c0fce89b0e5a146252470fd0a156
Peter <togipete@hotmail.com>
2013-08-05 19:18:45 -0700

    Completed exercise 49

--- ex49/ex48/parser.py
+++ ex49/ex48/parser.py
@@ -1,21 +1,85 @@
+class ParserError(Exception):
+    pass
+
+
+class Sentence(object):
+
+    def __init__(self, subject, verb, object):
+        # remember we take ('noun', 'princess') tuples and convert them
+        self.subject = subject[1]
+        self.verb = verb[1]
+        self.object = object[1]
+
+
 def peek(word_list):
-    if word_list:
-        word = word_list[0]
-        return word[0]
+    if word_list:  # check if there's any words left
+        word = word_list[0]  # word is the first tuple
+        return word[0]  # return first element (type) of the tuple
     else:
         return None
 
+
 def match(word_list, expecting):
     if word_list:
-        word = word_list.pop(0)
+        word = word_list.pop(0)  # pop the first element
 
+        # word is a tuple at this point
+        # word[0] is the word type
         if word[0] == expecting:
-            return word
+            return word  # return the tuple if the type matches
         else:
             return None
     else:
         return None
 
+
 def skip(word_list, word_type):
+    # peek returns the type of the first word in the list
+    # if it's of the target type, pop it from the word list (done in match)
+    # moves to the next tuple until it's not of the target type
     while peek(word_list) == word_type:
         match(word_list, word_type)
+
+
+def parse_verb(word_list):
+    skip(word_list, 'stop')
+
+    if peek(word_list) == 'verb':
+        return match(word_list, 'verb')
+    else:
+        raise ParserError("Expected a verb next.")
+
+
+def parse_object(word_list):
+    skip(word_list, 'stop')
+    next = peek(word_list)
+
+    if next == 'noun':
+        return match(word_list, 'noun')
+    if next == 'direction':
+        return match(word_list, 'direction')
+    else:
+        raise ParserError("Expected a noun or direction next.")
+
+
+def parse_subject(word_list, subj):
+    # subject already identified/passed when entering this function (from parse_sentence)
+    verb = parse_verb(word_list)
+    obj = parse_object(word_list)
+
+    return Sentence(subj, verb, obj)
+
+
+def parse_sentence(word_list):
+    skip(word_list, 'stop')
+
+    start = peek(word_list)
+
+    if start == 'noun':
+        subj = match(word_list, 'noun')
+        return parse_subject(word_list, subj)
+    elif start == 'verb':
+        # assume the subject is the player then
+        return parse_subject(word_list, ('noun', 'player'))
+    else:
+        raise ParserError("Must start with the subject, object, or verb not: %s" % start)
---
+++ ex49/tests/parser_tests.py
@@ -0,0 +1,86 @@
+from nose.tools import *
+from ex48 import parser
+
+
+def sentence_tests():
+    sentence = parser.Sentence(('subject', 'asubject'), ('verb', 'averb'), ('object', 'aobject'))
+    assert_equals(sentence.subject, 'asubject')
+    assert_equals(sentence.verb, 'averb')
+    assert_equals(sentence.object, 'aobject')
+
+
+def peek_tests():
+    word_list = [('type_a', 'word_a'), ('type_b', 'word_b')]
+    assert_equals(parser.peek(word_list), 'type_a')
+
+    word_list = [('type_b', 'word_b')]
+    assert_equals(parser.peek(word_list), 'type_b')
+
+    word_list = []
+    assert_equals(parser.peek(word_list), None)
+
+
+def match_tests():
+    word_list = [('type_a', 'word_a'), ('type_b', 'word_b')]
+    assert_equals(parser.match(word_list, 'type_a'), ('type_a', 'word_a'))
+    assert_equals(parser.match(word_list, 'type_b'), ('type_b', 'word_b'))
+    assert_equals(parser.match(word_list, 'type_c'), None)
+
+
+def parse_skip_tests():
+    word_list = [('type_a', 'word_a'), ('type_a', 'word_a'), ('type_b', 'word_b')]
+    parser.skip(word_list, 'type_a')
+    assert_equals(parser.peek(word_list), 'type_b')
+    parser.skip(word_list, 'type_b')
+    assert_equals(parser.peek(word_list), None)
+
+
+def parse_verb_tests():
+    word_list = [('stop', 'stop_a'), ('verb', 'verb_a'), ('noun', 'noun_a')]
+    assert_equals(parser.parse_verb(word_list), ('verb', 'verb_a'))
+    assert_raises(parser.ParserError, parser.parse_verb, word_list)
+    assert_equals(word_list, [('noun', 'noun_a')])
+
+
+def parse_object_tests():
+    word_list = [('stop', 'stop_a'), ('verb', 'verb_a'), ('noun', 'noun_a'), ('direction', 'direction_a')]
+    assert_raises(parser.ParserError, parser.parse_object, word_list)
+    parser.parse_verb(word_list)
+    assert_equals(word_list, [('noun', 'noun_a'), ('direction', 'direction_a')])
+    assert_equals(parser.parse_object(word_list), ('noun', 'noun_a'))
+    assert_equals(parser.parse_object(word_list), ('direction', 'direction_a'))
+
+
+def parse_subject_tests():
+    word_list = [('verb', 'eat'), ('stop', 'the'), ('noun', 'bear')]
+    sentence = parser.parse_subject(word_list, ('noun', 'The Subject'))
+    assert_equals(sentence.subject, 'The Subject')
+    assert_equals(sentence.verb, 'eat')
+    assert_equals(sentence.object, 'bear')
+
+    word_list = [('noun', 'bee'), ('verb', 'stung'), ('stop', 'the'), ('noun', 'human')]
+    # in real usage, first tuple should have matched/been popped already
+    assert_raises(parser.ParserError, parser.parse_subject, word_list, ('noun', 'bee'))
+    word_list = [('verb', 'stung'), ('stop', 'the'), ('adj', 'freaking'), ('noun', 'human')]
+    # extra in front of the object
+    assert_raises(parser.ParserError, parser.parse_subject, word_list, ('noun', 'bee'))
+
+
+def parse_sentence_tests():
+    word_list = [('stop', 'the'), ('adj', 'damned'), ('noun', 'bee'), ('verb', 'stung'), ('stop', 'the'), ('noun', 'human')]
+    assert_raises(parser.ParserError, parser.parse_sentence, word_list)
+
+    word_list = [('stop', 'the'), ('noun', 'bee'), ('verb', 'stung'), ('stop', 'the'), ('noun', 'human')]
+    sentence = parser.parse_sentence(word_list)
+    assert_equals(sentence.subject, 'bee')
+    assert_equals(sentence.verb, 'stung')
+    assert_equals(sentence.object, 'human')
+
+    word_list = [('verb', 'stung'), ('stop', 'the'), ('noun', 'human')]
+    sentence = parser.parse_sentence(word_list)
+    assert_equals(sentence.subject, 'player')
+    assert_equals(sentence.verb, 'stung')
+    assert_equals(sentence.object, 'human')
+
+    word_list = [('start', 'hello'), ('noun', 'bee'), ('verb', 'stung'), ('stop', 'the'), ('noun', 'human')]
+    assert_raises(parser.ParserError, parser.parse_sentence, word_list)
